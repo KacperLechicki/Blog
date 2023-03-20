9194a65b9a531632ec9c0e43433931331c32c301
kacperlechicki <klechicki@poczta.fm>
2023-03-21 00:49:46 +0100

    connected categories from backend to frontend

--- src/app/layouts/category-nav/category-nav.component.ts
+++ src/app/layouts/category-nav/category-nav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CategoriesService } from 'src/app/services/categories.service';
 
 @Component({
   selector: 'app-category-nav',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./category-nav.component.scss']
 })
 export class CategoryNavComponent {
+
+  categoryArray: any[] = [];
+
+  constructor(private categoriesService: CategoriesService) {}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.categoriesService.loadData().subscribe((val:any) => {
+      this.categoryArray = val;
+    })
+  }
 
 }
