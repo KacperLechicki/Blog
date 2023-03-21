d867db1324a5957bb1de81c69ddbeb174c927274
kacperlechicki <klechicki@poczta.fm>
2023-03-21 11:18:52 +0100

    image preview

--- src/app/layouts/new-post/new-post.component.ts
+++ src/app/layouts/new-post/new-post.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CategoriesService } from 'src/app/services/categories.service';
+import { LoadingService } from 'src/app/services/loading.service';
 
 @Component({
   selector: 'app-new-post',
@@ -6,8 +8,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-post.component.scss'],
 })
 export class NewPostComponent {
-  onTitleChange($event: any) {
-    console.log($event);
+  permalink: string = '';
+  categoryArray: any[] = [];
+  loading: boolean = false;
+  imgSrc: any = './assets/placeholder-img.jpg';
+
+  constructor(
+    private categoriesService: CategoriesService,
+    private loadingS: LoadingService
+  ) {}
+
+  ngOnInit(): void {
+    this.loading = this.loadingS.loadingStart();
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.categoriesService.loadData().subscribe((val: any) => {
+      this.categoryArray = val;
+    });
+    this.loading = this.loadingS.loadingStop();
+  }
+
+  onTitleChange($event: any): void {
+    const title = $event.target.value;
+    this.permalink = title.replace(/\s/g, '-');
+  }
+
+  showPreview($event: any): void {
+    const reader = new FileReader();
+    this.loading = this.loadingS.loadingStart();
+    reader.onload = (e: any) => {
+      this.imgSrc = e.target.result;
+    };
+
+    reader.readAsDataURL($event.target.files[0]);
+
+    setTimeout(() => {
+      this.loading = this.loadingS.loadingStop();
+    }, 1000);
   }
 
   moveUp(): void {
